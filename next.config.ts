6bb982e48fcc3db614e5883589ecdac0f354641a
azurecoders <azfarchannel54321@gmail.com>
2025-06-02 02:18:05 +0500

    Fixed the images issue

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,10 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    domains: ["ik.imagekit.io", "images.unsplash.com", "plus.unsplash.com"],
-    loader: "default",
-  },
+  // images: {
+  //   domains: ["ik.imagekit.io", "images.unsplash.com", "plus.unsplash.com"],
+  //   loader: "default",
+  // },
 };
 
 export default nextConfig;
