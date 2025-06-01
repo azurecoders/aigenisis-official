72612e4e9a28d9eb901a5a2ce48cb3179a3ca275
azurecoders <azfarchannel54321@gmail.com>
2025-06-02 02:25:22 +0500

    Fixed next config file

--- next.config.ts
+++ next.config.ts
@@ -13,8 +13,6 @@ const nextConfig: NextConfig = {
         pathname: "/**",
       },
     ],
-    domains: ["ik.imagekit.io", "images.unsplash.com", "plus.unsplash.com"],
-    loader: "default",
   },
   // images: {
   //   domains: ["ik.imagekit.io", "images.unsplash.com", "plus.unsplash.com"],
